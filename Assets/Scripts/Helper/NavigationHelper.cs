2b4c7d0f73e17c851bedb3cb6806a0513e8244e9
Bugsyaya <marina.blin@free.fr>
2018-01-27 12:57:43 +0100

    WIP

---
+++ Assets/Scripts/Helper/NavigationHelper.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class NavigationHelper : MonoBehaviour
+{
+  public void PlayGame(string lvl)
+  {
+    SceneManager.LoadScene("lvl" + lvl);
+  }
+
+  public void TutoGame()
+  {
+    SceneManager.LoadScene("Tuto");
+  }
+
+  public void WinGame()
+  {
+    SceneManager.LoadScene("Win");
+  }
+
+  public void LooseGame()
+  {
+    SceneManager.LoadScene("Loose");
+  }
+
+  public void CreditGame()
+  {
+    SceneManager.LoadScene("Credit");
+  }
+
+  public void LvlMenuGame()
+  {
+    SceneManager.LoadScene("lvlMenu");
+  }
+
+  public void MainGame()
+  {
+    SceneManager.LoadScene("Start");
+  }
+}
