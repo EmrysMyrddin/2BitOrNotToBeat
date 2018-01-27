d605205b89be318a969d12d0917c9aaaaf529b7e
Valentin Cocaud <v.cocaud@gmail.com>
2018-01-27 12:48:26 +0100

    :sparkles: Draw path with direction

--- Assets/Scripts/CableDrawer.cs
+++ Assets/Scripts/CableDrawer.cs
@@ -3,73 +3,103 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CableDrawer : MonoBehaviour {
-	private static int COLS = 20, ROWS = 10;
-	public GameObject CellPrefab;
+public class CableDrawer : MonoBehaviour
+{
+    private static int COLS = 20, ROWS = 10;
+    public GameObject CellPrefab;
 
-	private Plane plane = new Plane(Vector3.back, Vector3.zero);
-	private Cell[,] grid = new Cell[COLS,ROWS];
-	private Vector2Int currentPos;
-	private Stack<Vector2Int> currentPath = new Stack<Vector2Int>();
+    private Plane plane = new Plane(Vector3.back, Vector3.zero);
+    private Cell[,] grid = new Cell[COLS, ROWS];
+    private Vector2Int currentPos;
+    private Stack<Vector2Int> currentPath = new Stack<Vector2Int>();
 
-	// Use this for initialization
-	void Start () {
-		for(int x = 0 ; x < COLS ; x++) {
-			for(int y = 0 ; y < ROWS ; y++) {
-				GameObject newCell = Instantiate(CellPrefab, new Vector3((float)x, (float)y, 0f), Quaternion.identity);
-				grid[x, y] = newCell.GetComponent<Cell>();
-			}
-		}
-	}
+    // Use this for initialization
+    void Start()
+    {
+        for (int x = 0; x < COLS; x++)
+        {
+            for (int y = 0; y < ROWS; y++)
+            {
+                GameObject newCell = Instantiate(CellPrefab, new Vector3((float)x, (float)y, 0f), Quaternion.identity);
+                grid[x, y] = newCell.GetComponent<Cell>();
+            }
+        }
+    }
 
-	void OnMouseDown() {
-		Vector2Int mousePos = MousePosition();
-		currentPos = mousePos;
-		currentPath = new Stack<Vector2Int>();
-		grid[mousePos.x, mousePos.y].SetIsCable(true);
-	}
+    void OnMouseDown()
+    {
+        Vector2Int mousePos = MousePosition();
+        currentPos = mousePos;
+        currentPath = new Stack<Vector2Int>();
+        GetCell(currentPos).SetIsCable(true);
+        GetCell(currentPos).SetIsCurrent(true);
+    }
 
-	void OnMouseDrag() {
-		Vector2Int mousePos = MousePosition();
-		if(mousePos.x >= COLS || mousePos.y >= ROWS || mousePos.x < 0 || mousePos.y < 0) return;
+    void OnMouseUp() {
+        GetCell(currentPos).SetIsCurrent(false);
+    }
 
-		// Calculate the distance from the current cell
-		float dx = Math.Abs(mousePos.x - currentPos.x);
-		float dy = Math.Abs(mousePos.y - currentPos.y);
+    void OnMouseDrag()
+    {
+        Vector2Int mousePos = MousePosition();
+        if (mousePos.x >= COLS || mousePos.y >= ROWS || mousePos.x < 0 || mousePos.y < 0) return;
 
-		// If the mouse has mooved to a new cell
-		if( dx + dy == 1) {
-			if(currentPath.Count > 0 && mousePos == currentPath.Peek()) {
-				GetCell(currentPos).SetIsCable(false);
-				currentPath.Pop();
-				grid[currentPos.x, currentPos.y].SetIsCurrent(false);
-				grid[mousePos.x, mousePos.y].SetIsCurrent(true);
-				currentPos = mousePos;
-			} else if(!GetCell(mousePos).isCable) {
-				GetCell(mousePos).SetIsCable(true);
-				currentPath.Push(currentPos);
-				grid[currentPos.x, currentPos.y].SetIsCurrent(false);
-				grid[mousePos.x, mousePos.y].SetIsCurrent(true);
-				currentPos = mousePos;
-			}
+        // Calculate the distance from the current cell
+        Vector2Int distance = mousePos - currentPos;
 
-			
-		}
-	}
-	
-	Cell GetCell(Vector2Int pos) {
-		return grid[pos.x, pos.y];
-	}
+        // If the mouse has mooved to a new cell
+        if (distance.magnitude == 1)
+        {
+            // Detect if we havec moved backward
+            if (currentPath.Count > 0 && mousePos == currentPath.Peek())
+            {
+                CancelLastCable(mousePos);
+            }
+            else if (!GetCell(mousePos).isCable)
+            {
+                CreateNewCable(mousePos, distance);
+            }
+        }
+    }
 
-	Vector3 RoundVector3(Vector3 vector) {
-		return new Vector3(Mathf.Floor(vector.x), Mathf.Floor(vector.y), Mathf.Floor(vector.z));
-	}
+    void CancelLastCable(Vector2Int mousePos)
+    {
+        GetCell(currentPos).SetIsCable(false);
+        currentPath.Pop();
+        grid[currentPos.x, currentPos.y].SetIsCurrent(false);
+        grid[mousePos.x, mousePos.y].SetIsCurrent(true);
+        currentPos = mousePos;
+    }
 
-	Vector2Int MousePosition() {
-		Vector2 screenPos = Input.mousePosition;
-		Ray ray=Camera.main.ScreenPointToRay(new Vector3(screenPos.x, screenPos.y, 1f));
-		float distance;
-		if(!plane.Raycast(ray, out distance)) throw new UnityException("did not hit plane");
-		return Vector2Int.FloorToInt(RoundVector3(ray.GetPoint(distance)));
-	}
+    void CreateNewCable(Vector2Int mousePos, Vector2Int direction)
+    {
+        Cell cell = GetCell(mousePos);
+        
+        cell.SetIsCable(true);
+        cell.SetDirection(direction);
+
+        currentPath.Push(currentPos);
+        grid[currentPos.x, currentPos.y].SetIsCurrent(false);
+        grid[mousePos.x, mousePos.y].SetIsCurrent(true);
+        currentPos = mousePos;
+    }
+
+    Cell GetCell(Vector2Int pos)
+    {
+        return grid[pos.x, pos.y];
+    }
+
+    Vector3 RoundVector3(Vector3 vector)
+    {
+        return new Vector3(Mathf.Floor(vector.x), Mathf.Floor(vector.y), Mathf.Floor(vector.z));
+    }
+
+    Vector2Int MousePosition()
+    {
+        Vector2 screenPos = Input.mousePosition;
+        Ray ray = Camera.main.ScreenPointToRay(new Vector3(screenPos.x, screenPos.y, 1f));
+        float distance;
+        if (!plane.Raycast(ray, out distance)) throw new UnityException("did not hit plane");
+        return Vector2Int.FloorToInt(RoundVector3(ray.GetPoint(distance)));
+    }
 }
--- Assets/Scripts/Cell.cs
+++ Assets/Scripts/Cell.cs
@@ -2,22 +2,55 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Cell : MonoBehaviour {
-	public Sprite CableSprite;
-	public bool isCable = false;
+public class Cell : MonoBehaviour
+{
+    public Sprite Cable, CableUp, CableDown, CableRight, CableLeft;
+    public Dictionary<string, Sprite> CableSprites = new Dictionary<string, Sprite>();
+    public bool isCable = false;
+    public Vector2Int direction;
 
-	private SpriteRenderer spriteRenderer;
+    private SpriteRenderer spriteRenderer;
+	private string[] directions = new string[]{
+		"Right",
+		"Left",
+		"Up",
+		"Down",
+		"UpRight",
+		"UpLeft",
+		"DownRight",
+		"DownLeft",
+	};
 
-	void Start() {
-		spriteRenderer = GetComponent<SpriteRenderer>();
-	}
+    void Start()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
 
-	public void SetIsCable(bool isCable) {
-		spriteRenderer.sprite = isCable ? CableSprite : null;
-		this.isCable = isCable;
-	}
+    public void SetIsCable(bool isCable)
+    {
+        this.isCable = isCable;
+        if (isCable)
+        {
+            spriteRenderer.sprite = Cable;
+        }
+        else
+        {
+            direction = Vector2Int.zero;
+            spriteRenderer.sprite = null;
+        }
+    }
 
-	public void SetIsCurrent(bool isPrevious) {
-		spriteRenderer.material.color = isPrevious ? Color.blue : Color.white;
-	}
+    public void SetIsCurrent(bool isPrevious)
+    {
+        spriteRenderer.material.color = isPrevious ? Color.grey : Color.white;
+    }
+
+    public void SetDirection(Vector2Int direction)
+    {
+        this.direction = direction;
+        if (direction.x > 0) spriteRenderer.sprite = CableRight;
+        if (direction.x < 0) spriteRenderer.sprite = CableLeft;
+        if (direction.y > 0) spriteRenderer.sprite = CableUp;
+        if (direction.y < 0) spriteRenderer.sprite = CableDown;
+    }
 }
